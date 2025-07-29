58b5e2936ebb9ca43c913e51f72f545dc3b96326
FlyPathic <ashybaseball88@gmail.com>
2025-07-29 01:21:50 -0500

    Increase jump velocity and add respawn logic
    Raised the player's jump velocity for higher jumps and implemented a respawn mechanism when the player falls below y=600. Updated initial player position and added new platforms to the main scene for expanded level design.

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 
 
 func _physics_process(delta: float) -> void:
@@ -10,10 +10,13 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if position.y > 600:
+		position.x = 629
+		position.y = 339
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
