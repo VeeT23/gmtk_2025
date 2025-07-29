71c2a7242cc38bdb94e104aa38d76a5a6287f98e
VeeT23 <josephtobias239@gmail.com>
2025-07-29 02:32:56 -0500

    groups

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
+var id
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
---
+++ assets/world/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var player_resource = preload("res://assets/player/player.tscn")
+
+func spawn_player(id: int):
+	var player_scene = player_resource.instantiate()
+	player_scene.id = id
+	add_child(player_scene)
+
+func remove_player(id: int):
+	for child in get_children():
+		if child.is_in_group("Player"):
+			if child.id = id:
+				child.queue_free()
