0125688feb4b4c1d4034db4782e4436bec0e9c69
VeeT23 <josephtobias239@gmail.com>
2025-07-28 20:47:43 -0500

    new menu

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,10 +1,41 @@
-extends Control
+extends Node2D
+
+const transition_time : float = 0.5
 
 const main_scene_path : String = "res://assets/world/main.tscn"
+const button_size : Rect2 = Rect2(-46,-12,92,24)
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file(main_scene_path)
+@onready var start_button : Sprite2D = $StartButton
+@onready var load_button : Sprite2D = $LoadButton
+@onready var options_button : Sprite2D = $OptionsButton
+@onready var quit_button : Sprite2D = $QuitButton
+@onready var animation_player : AnimationPlayer = $CanvasLayer/Overlay/Fade/AnimationPlayer
 
+func _ready() -> void:
+	animation_player.speed_scale = 1.0 / transition_time
 
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
+func is_mouse_over_button(button_node : Sprite2D, mouse_position : Vector2) -> bool:
+	return button_size.has_point(button_node.to_local(mouse_position))
+
+func _input(event: InputEvent) -> void: 
+	var mouse_position = get_global_mouse_position()
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if is_mouse_over_button(start_button, mouse_position):
+			start_button.frame_coords.x = 2
+			animation_player.play("fade_out")
+			await get_tree().create_timer(transition_time).timeout
+			get_tree().change_scene_to_file(main_scene_path)
+		elif is_mouse_over_button(load_button, mouse_position):
+			load_button.frame_coords.x = 2 #TODO: add load feature
+		elif is_mouse_over_button(options_button,mouse_position):
+			options_button.frame_coords.x = 2 #TODO: add options
+		elif is_mouse_over_button(quit_button, mouse_position):
+			quit_button.frame_coords.x = 2
+			animation_player.play("fade_out")
+			await get_tree().create_timer(transition_time).timeout
+			get_tree().quit()
+		
+	elif event is InputEventMouseMotion:
+		for button in [start_button, load_button, options_button, quit_button]:
+			if button.frame_coords.x != 2:
+				button.frame_coords.x = int(!is_mouse_over_button(button, mouse_position))
