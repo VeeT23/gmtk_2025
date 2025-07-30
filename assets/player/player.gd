cc196e825b32ef01de3264a65f89944e47c2ae7f
VeeT23 <josephtobias239@gmail.com>
2025-07-30 12:36:57 -0500

    top down

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -13,30 +13,24 @@ func _ready() -> void:
 		$Camera2D.make_current()
 
 func _physics_process(delta: float) -> void:
-	if is_multiplayer_authority():
-		# Apply gravity
-		if not is_on_floor():
-			velocity.y += get_gravity().y * delta
-
-		# Respawn if too low
-		if position.y > 600:
-			position = Vector2(629, 339)
-
-		# Jump
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-
-		# Horizontal movement
-		var direction := Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		# Apply motion and sync position to others
-		move_and_slide()
-		rpc("sync_position", global_position)
+	if not is_multiplayer_authority(): return
 	
+	
+	var direction : Vector2 = Vector2(Input.get_axis("left", "right"),Input.get_axis("up", "down") )
+	if direction.x:
+		velocity.x = direction.x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction.y:
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	# Apply motion and sync position to others
+	move_and_slide()
+	rpc("sync_position", global_position)
+
 
 @rpc("any_peer")
 func sync_position(pos: Vector2):
