72d558e873976586253e6c4a1c87bb8d3554a150
VeeT23 <josephtobias239@gmail.com>
2025-07-30 19:00:53 -0500

    oops

--- assets/world/world.gd
+++ assets/world/world.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var day_duration_minutes = 4.0
+var day_duration_minutes = 0.25
 
 var player_scene: PackedScene = preload("res://assets/player/player.tscn")
 
@@ -14,7 +14,6 @@ func _ready() -> void:
 		Network.create_server()
 	else:
 		Network.join_server()
-	
 	$DayNightTimer.start(day_duration_minutes * 60.0)
 
 func spawn_player(id: int):
@@ -45,4 +44,4 @@ func remove_player(id: int):
 		players.erase(id)
 
 func _process(_delta: float) -> void:
-	pass
+	$DirectionalLight2D.color.a = remap($DayNightTimer.time_left, day_duration_minutes * 60.0,0.0,0.0,1.0)
