0855da9a622a06ddcc143433702b5f13271bb748
VeeT23 <josephtobias239@gmail.com>
2025-07-31 16:47:38 -0500

    branch

--- assets/trap/trap.gd
+++ assets/trap/trap.gd
@@ -5,16 +5,8 @@ var transition_time: float = 4.0
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
-		animate()
+		$Sprite2D.frame = 1
 		body.actual_speed = 0
 		await get_tree().create_timer(transition_time).timeout
 		body.actual_speed = body.SPEED
 		queue_free()
-
-
-func animate():
-	$Sprite2D.visible = false
-	$Frame2.visible = true
-	await get_tree().create_timer(0.2).timeout
-	$Frame2.visible = false
-	$Frame3.visible = true
