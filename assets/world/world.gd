e3836fa578c1a296a70880f3e8a3c0cd54608b7e
VeeT23 <josephtobias239@gmail.com>
2025-07-31 22:50:19 -0500

    fix

--- assets/world/world.gd
+++ assets/world/world.gd
@@ -48,11 +48,12 @@ func remove_player(id: int):
 		players.erase(id)
 
 func reset_scene():
+	current_day += 1
 	$CanvasLayer/Announcement.announce("Day " + str(current_day))
 	await  get_tree().create_timer(1).timeout
 	get_tree().call_group("Loot", "reset")
 	$DayNightTimer.start(DAY_DURATION_MINUTES * 60.0)
-	current_day += 1
+	
 	
 
 func _process(_delta: float) -> void:
