f580ddeeee5eaf83998c3c85c3229a91346b71c3
FlyPathic <ashybaseball88@gmail.com>
2025-07-31 21:22:54 -0500

    Update campfire interaction and add more branches
    Changed campfire interaction to trigger on 'interact' release and increment branch count by 1 instead of all branches. Added multiple new branch nodes to the world scene for increased resource availability.

--- assets/campfire/campfire.gd
+++ assets/campfire/campfire.gd
@@ -8,12 +8,13 @@ func _ready() -> void:
 	$Sprite2D/AnimationPlayer.play("fire")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"):
+	if event.is_action_released("interact"):
 		for body in $Area2D.get_overlapping_bodies():
 			if body.is_in_group("Player"):
 				if body.is_multiplayer_authority():
 					if "branch" in get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory and get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory["branch"] >= 1:
-						branches["branch"] += get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory["branch"]
+						print("ADDED")
+						branches["branch"] += 1
 						get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory["branch"] -= 1
 						$PointLight2D.texture_scale += 1
 						$Label.text = "Branches: " + str(branches["branch"])
