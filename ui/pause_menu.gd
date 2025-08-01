70058516f5c4d915b8d136049a073fac1760c3ff
FlyPathic <ashybaseball88@gmail.com>
2025-07-31 23:30:07 -0500

    Improve pause menu functionality and update asset import
    Set process_mode to always for the pause menu and ensure game pausing/unpausing is handled when showing or hiding the menu. Updated PlayerSprite asset import to use correct file extension. Minor cleanup in lobby.tscn scene header.

--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -3,16 +3,20 @@ extends Control
 const main_menu_path : String = "res://ui/main_menu.tscn"
 
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	hide() 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape_menu"): #Escape button
 		if visible:
 			hide()
+			get_tree().paused = false
 		else:
 			show()
+			get_tree().paused = true
 
 func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
 	hide()
 
 func _on_save_button_pressed() -> void:
