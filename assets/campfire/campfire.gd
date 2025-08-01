908c72bc0d917e2957b1f0bae6fd6025307f1c6b
FlyPathic <ashybaseball88@gmail.com>
2025-07-31 19:31:17 -0500

    Add campfire scene with interaction and UI
    Introduced a new campfire.gd script and updated the campfire.tscn scene to support player interaction for adding branches from inventory. Added UI elements to display branch count, a PointLight2D for visual effect, and expanded collision and area detection for interaction.

---
+++ assets/campfire/campfire.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+var branches = {
+	"branch": 0
+}
+
+func _ready() -> void:
+	$Sprite2D/AnimationPlayer.play("fire")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		for body in $Area2D.get_overlapping_bodies():
+			if body.is_in_group("Player"):
+				if body.is_multiplayer_authority():
+					
+						if "branch" in get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory and get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory["branch"] >= 1:
+							branches["branch"] += get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory["branch"]
+							get_tree().get_root().get_node("World/CanvasLayer/Inventory").inventory["branch"] -= 1
+							$Label.text = "Branches: " + str(branches["branch"])
+							get_tree().get_root().get_node("World/CanvasLayer/Inventory").update_item_list()
+				rpc("sync_looted_open")
+				break
